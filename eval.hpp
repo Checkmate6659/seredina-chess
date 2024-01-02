0372c1ad4a1f3b47e4779531da6653299f2d71b7
EnigmaChess6659 <73489554+Checkmate6659@users.noreply.github.com>
2024-01-02 15:28:55 +0100

    Added tempo & passer by rank, all texel tuned

--- eval.hpp
+++ eval.hpp
@@ -6,7 +6,15 @@
 #include "chess.hpp"
 using namespace chess;
 
+//pack 2 16-bit signed values into a 32-bit value
+#define S(mg__, eg__) (((uint16_t)(mg__) << 16) | ((uint16_t)(eg__) & 0xFFFF))
+//extract MG value from a S pair (upper 16 bits)
+#define MG(s_pair__) ((int16_t)((s_pair__) >> 16))
+//extract EG value from a S pair (lower 16 bits)
+#define EG(s_pair__) ((int16_t)(s_pair__))
+
 typedef int32_t Value;
+typedef int32_t ValPair; //for S pairs (signed so that we can narrow things properly)
 
 void init_tables();
 Value eval(Board board);
