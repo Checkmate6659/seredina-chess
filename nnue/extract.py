e60c8cc077b37411850c8397fe430a88c5bf6c3c
EnigmaChess6659 <73489554+Checkmate6659@users.noreply.github.com>
2024-01-07 15:44:37 +0100

    Extracted values from f32.bin, the sizes are good

---
+++ nnue/extract.py
@@ -0,0 +1,20 @@
+#extraction command: python3 extract.py > extracted.txt
+
+import numpy as np
+
+# Read in data that has been stored as raw 32-bit float samples
+raw = list(np.fromfile("f32.bin", dtype="float32"))
+print("Raw data:")
+print(raw)
+
+print("\nL1 Weights (stored column-major):")
+print(raw[:768*16]) #matrix is 768*16 (stored column-major)
+print("L1 Biases:")
+print(raw[768*16 : 768*16 + 16]) #16 biases
+
+print("L2 Weights:")
+print(raw[768*16 + 16 : 768*16 + 16 + 32]) #matrix is 32*1
+print("L2 Bias:")
+print(raw[768*16 + 16 + 32]) #single value
+print(len(raw), 768*16 + 16 + 32 + 1)
+
